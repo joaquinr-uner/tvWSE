4c485f62a4c60e31836ea3d6fe43106a2319dab1
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2022-04-11 11:32:14 -0300

    Corrected order of output arguments

--- create_bounds.m
+++ create_bounds.m
@@ -3,7 +3,9 @@ function [lb,ub] = create_bounds(vnv,D,N)
     lb = zeros(1,2*sum(vnv));
     for i=1:D-1
        lbi = [ones(1,vnv(i)-2) -Inf*ones(1,vnv(i)+2)];
+       %lbi = [ones(1,vnv(i)-2) zeros(1,vnv(i)) -Inf*ones(1,2)];
        ubi = [N*ones(1,vnv(i)-2) Inf*ones(1,vnv(i)+2)];
+       
        lb(2*(sum(vnv(1:i-1)))+1:2*sum(vnv(1:i))) = lbi;
        ub(2*(sum(vnv(1:i-1)))+1:2*sum(vnv(1:i))) = ubi;
        
