c08b6e54a46f00dc34147d65dd87d8703585b81e
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2022-04-13 13:50:02 -0300

    Corrected modal amplitude computation

--- compute_ampl_coefs.m
+++ compute_ampl_coefs.m
@@ -1,21 +1,18 @@
-function [a, b, alp, v] = compute_ampl_coefs(ti_h,Al,gamh,eh,N,D,nv)
-
-if length(nv) == 1
-    vnv = nv*ones(1,D-1);
-else
-    vnv = nv;
-end
+function [a, b, alp, v] = compute_ampl_coefs(ti_h,Al,gamh,eh,N,D,vlr,B1)
 
 alp = zeros(D-1,N);
 a = sum(Al,2)/N;
 b = gamh.*a';
 v = [];
 for i=1:D-1
-    alp(i,:) = Al(i,:)/a(i);
+    alp(i,:) = (Al(i,:)./B1)/a(i);
     aux = [ti_h{i} alp(i,ti_h{i}) a(i) b(i) eh(i)];
     v = [v aux];
 end
-
+a1 = sum(B1)/N;
+a = [a1; a];
+b = [0 b];
+v = [v vlr(1) 0];
 
 end
 
