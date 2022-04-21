87408b8079c12918952af9e658954e06ddde2eb5
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2022-04-21 16:19:20 -0300

    Corrected amplitude estimation

--- compute_ampl_coefs.m
+++ compute_ampl_coefs.m
@@ -1,18 +1,19 @@
-function [a, b, alp, v] = compute_ampl_coefs(ti_h,Al,gamh,eh,N,D,vlr,B1)
+function [a, b, O, alp] = compute_ampl_coefs(ti_h,Ql,gamh,N,D,nv,B1,mInterp)
 
-alp = zeros(D-1,N);
-a = sum(Al,2)/N;
-b = gamh.*a';
+alp = zeros(D-1,nv);
+O = zeros(D-1,N);
 v = [];
+a = zeros(1,D);
+b = zeros(1,D);
+a(1) = sum(B1)/N;
 for i=1:D-1
-    alp(i,:) = (Al(i,:)./B1)/a(i);
-    aux = [ti_h{i} alp(i,ti_h{i}) a(i) b(i) eh(i)];
-    v = [v aux];
+    ti = ti_h{i};
+    o = Ql{i};
+    b1 = interp1(1:N,B1,ti,mInterp);
+    O(i,:) = interp1(ti,o./b1,1:N,mInterp);
+    a(i+1) = sum(O(i,:),2)/N;
+    b(i+1) = gamh(i).*a(i+1);
+    alp(i,:) = (o./b1)/a(i+1);
 end
-a1 = sum(B1)/N;
-a = [a1; a];
-b = [0 b];
-v = [v vlr(1) 0];
-
 end
 
