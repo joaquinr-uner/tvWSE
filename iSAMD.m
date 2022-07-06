6f28ab37969e186c7e61246500259361df2d27d7
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2022-07-06 14:56:10 -0300

    Added condition for extended signal

--- iSAMD.m
+++ iSAMD.m
@@ -1,20 +1,25 @@
-function [x, v_e,exitflag] = iSAMD(f,A,phi,D,vnv,vh,mInterp,lb,ub,method,options)
+function [x, v_e,exitflag] = iSAMD(f,A,phi,D,vnv,vh,mInterp,lb,ub,method,ext,options)
+if nargin<11
+    ext = 0;
+end
 
-if nargin<11 && method == 0
+if nargin<12 && method == 0
     options = statset;
     options.RobustWgtFun = 'cauchy';
     options.MaxIter = 3000;
 else
-    if nargin<11 && method == 1
+    if nargin<12 && method == 1
         options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt','MaxFunctionEvaluations',400*2*sum(vnv),'Display','off');
     end
 end
 
 v0 = vh;
 X = [A;phi];
-N = length(f);
-modelfun = @(v,X)(regresion_reform(v,X,N,D,vnv,mInterp));
-
+if ext == 1
+    modelfun = @(v,X)(regresion_ext(v,X,D,vnv,mInterp));
+else
+    modelfun = @(v,X)(regresion_reform(v,X,D,vnv,mInterp));
+end
 warning('off')
 if method == 0
     fprintf('Using nlinfit...\n')
@@ -29,14 +34,13 @@ warning('on')
 
 
 x = modelfun(v_e,X);
-
 end
-function s = regresion_reform(v,X,N,D,vnv,mInterp)
+function s = regresion_reform(v,X,D,vnv,mInterp)
 I = length(D);
 A = X(1,:);
 phi = X(2,:);
 s = A.*cos(2*pi*phi);
-
+N = length(A);
 for i=1:D-1
     v_l = v(2*(sum(vnv(1:i-1)))+1:2*sum(vnv(1:i)));
     t_l = v_l(1:vnv(i)-2);
@@ -48,5 +52,27 @@ for i=1:D-1
     s = s + amp.*(cos(2*pi*e*phi)+ gam*sin(2*pi*e*phi));
     %subplot(2,1,i)
     %plot(amp)
+end
+end
+
+function s = regresion_ext(v,X,D,vnv,mInterp)
+I = length(D);
+A = X(1,:);
+phi = X(2,:);
+s = A.*cos(2*pi*phi);
+N = length(A);
+for i=1:D-1
+    v_l = v(2*(sum(vnv(1:i-1)+i))+1:2*sum(vnv(1:i))+4*i);
+    t_l = v_l(1:vnv(i));
+    A_l = v_l(vnv(i)+1:2*vnv(i)+2);
+    %amp = interp1([1, t_l, N],A_l,1:N,mInterp);
+    amp = interp1([0, t_l, 1],A_l,linspace(0,1,N),mInterp);
+    e = v_l(end);
+    gam = v_l(end-1);
+    s = s + amp.*(cos(2*pi*e*phi)+ gam*sin(2*pi*e*phi));
+    %subplot(2,1,i)
+    %plot(amp)
+    %title(num2str(e))
+    %pause(0.1)
 end
 end
