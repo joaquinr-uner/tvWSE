66dd4693872a17b4ee06af3eaaf3a04db631bf9f
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2022-07-07 11:04:08 -0300

    Corrected indexing of coef vector

--- create_bounds2.m
+++ create_bounds2.m
@@ -2,7 +2,7 @@ function [lb,ub] = create_bounds2(vnv,vh,D,N)
 ub = zeros(1,2*sum(vnv)+4*(D-1));
 lb = zeros(1,2*sum(vnv)+4*(D-1));
 for i=1:D-1
-    vhi = vh(2*(sum(vnv(1:i-1)+i))+1:2*sum(vnv(1:i))+4*i);
+    vhi = vh(2*sum(vnv(1:i-1))+4*(i-1)+1:2*sum(vnv(1:i))+4*i);
     t1i = vhi(1);
     t1f = vhi(vnv(i));
     %lbi = [1/N*ones(1,vnv(i)-2) zeros(1,vnv(i)) -Inf*ones(1,2)];
@@ -10,8 +10,8 @@ for i=1:D-1
     %lbi = [ones(1,vnv(i)-2) zeros(1,vnv(i)) -Inf*ones(1,2)];
     ubi = [t1i (t1f-1/N)*ones(1,vnv(i)-2) t1f Inf*ones(1,vnv(i)+4)];
     
-    lb(2*(sum(vnv(1:i-1)+i))+1:2*sum(vnv(1:i))+4*i) = lbi;
-    ub(2*(sum(vnv(1:i-1)+i))+1:2*sum(vnv(1:i))+4*i) = ubi;
+    lb(2*sum(vnv(1:i-1))+4*(i-1)+1:2*sum(vnv(1:i))+4*i) = lbi;
+    ub(2*sum(vnv(1:i-1))+4*(i-1)+1:2*sum(vnv(1:i))+4*i) = ubi;
     
 end
 
