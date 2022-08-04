205742d4c08fa3cf24f50c293e746873dec78ebe
joaquinr-uner <jruiz@ingenieria.uner.edu.ar>
2022-08-04 17:29:50 -0300

    Added new parameter for number of outer nodes

--- iSAMD_Coefs2.m
+++ iSAMD_Coefs2.m
@@ -1,26 +1,33 @@
-function vh = iSAMD_Coefs2(v,vnv,D,flag,N,Next,t_init)
+function vh = iSAMD_Coefs2(v,vnv,D,flag,N,Ne,t_init,outn)
 % Constructs the coefficient vector to be used in the iSAMD algorithm
 % with the extended signal
 if nargin<4
     flag = 1;
+end
+if nargin<5
+    N = 1;
+    Ne = 1;
+end
+if nargin<7
+    t_init = 0;
+end
+if nargin<8 && Ne/N>1
+    outn = 1;
 else
-    if nargin<5
-        N = 1;
-        Next = 1;
-    else
-        if nargin<7
-            t_init = 0;
-        end
+    if nargin<8 && Ne/N==1
+        outn = 0;
     end
 end
-vh = zeros(1,2*sum(vnv)+4*(D-1));
+
+r_ext = Ne/N;
+vh = zeros(1,2*sum(vnv)+4*outn*(D-1));
 vgam = v(end-D+1:end)./v(end-2*D+1:end-D);
 for i=2:D
     if flag == 1
-        A_l = v(i)*ones(1,vnv(i-1)+2);
+        A_l = v(i)*ones(1,vnv(i-1)+2*outn);
         gam_l = vgam(i);
     else
-        A_l = v(sum(vnv(1:i-2)+2)+1:sum(vnv(1:i-1)+2));
+        A_l = v(sum(vnv(1:i-2)+2*outn)+1:sum(vnv(1:i-1)+2*outn));
         %A_l = v(sum(vnv(1:i-2))+1:sum(vnv(1:i-1)));
         gam_l = vgam(i);
         %gam_l = 0;
@@ -29,13 +36,16 @@ for i=2:D
         inter = t_init{i-1};
     else
         %inter = floor(linspace(1,N,vnv(i-1)));
-        ti = ((Next-N+1)/2)/Next;
-        te = 1-((Next-N)/2)/Next;
-        inter = linspace(ti,te,vnv(i-1));
+        ti = 0.5*(1-1/r_ext+1/Ne);
+        te = 1-0.5*(1-1/r_ext);
+        outni = linspace(0,ti,outn+1);
+        outne = linspace(te,1,outn+1);
+        t_in = linspace(ti,te,vnv(i-1));
+        inter = [outni(2:end-1) t_in outne(2:end-1)];
         %inter = linspace(0,1,vnv(i-1));
     end
     t_l = inter;
     aux = [t_l,A_l,gam_l,i];
-    vh(2*(sum(vnv(1:i-2))+2*(i-2))+1:2*(sum(vnv(1:i-1))+2*(i-1))) = aux;
+    vh(2*(sum(vnv(1:i-2))+2*outn*(i-2))+1:2*(sum(vnv(1:i-1))+2*outn*(i-1))) = aux;
 end
 end
